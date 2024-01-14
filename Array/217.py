9c64e674c5e4cf06d752873c18589fd68b9a8aad
ahmedhassan-dev <ahmedhassan.op9@gmail.com>
2024-01-14 21:12:58 +0200

    Solve 217

---
+++ Array/217.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset = set()
+        for i in nums:
+            if i in hset:
+                return True
+            else:
+                hset.add(i)
+        return False
+
+        
